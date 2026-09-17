187da10ff73f7d5d53731aa8f54cdad034a06bcf
Emily-Capo <ecapodarco@my365.bellevue.edu>
2026-09-17 15:49:28 -0500

    Correct return statement and add age output
    Fix return statement and print age variable.

--- M2_discussion_capo.cpp
+++ M2_discussion_capo.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello, " << name << endl;
+
+   
+    retrn 0;
+
+
+    cout << age << endl;
+
+}
 
