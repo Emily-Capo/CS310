82e061cbd4a62cb6cf15badd9099bca636df9b19
Emily-Capo <ecapodarco@my365.bellevue.edu>
2026-09-24 16:29:23 -0500

    Add selection control and fix variable typo
    Fix syntax errors and improve output messages.

---
+++ Week 3/Discussion_capo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int value;
+
+    cout << "Enter a number: ";
+    cin >> value;
+
+    // Selection control statement
+    if (value > 10) 
+        cout << "Value is greater than 10!" << endl;
+    else 
+        cout << "Value is 10 or less!" << endl;
+  
+    if (valeu < 5) {
+        cout << "This will never run." << endl;
+    }
+
+    cout << "Another message" << endl
+
+    return 0;
+}
